69dd844f6d1dea8d6253bbb1d6b5276606e4ed16
osman2001sen <117099035+osman2001sen@users.noreply.github.com>
2023-03-19 01:30:35 -0400

    Randevu2

--- cohort3/triforciacohort3pack/force-app/main/default/lwc/randevu/randevu.js
+++ cohort3/triforciacohort3pack/force-app/main/default/lwc/randevu/randevu.js
@@ -9,8 +9,6 @@ export default class Randevu extends LightningElement {
 
   // https://triforciaprojectpackage-dev-ed.develop.my.site.com/s/randevu?calendlyLink={!get_user.Calendly__c}&amp;leadId=${!getLead.Id}&amp;leadName={!getLead.Name}&amp;leadEmail={!getLead.Email}
   
-  // Calendly token: eyJraWQiOiIxY2UxZTEzNjE3ZGNmNzY2YjNjZWJjY2Y4ZGM1YmFmYThhNjVlNjg0MDIzZjdjMzJiZTgzNDliMjM4MDEzNWI0IiwidHlwIjoiUEFUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJodHRwczovL2F1dGguY2FsZW5kbHkuY29tIiwiaWF0IjoxNjc4NzM3OTU1LCJqdGkiOiI1MTE0Y2UwMy00YzcyLTRkMmYtODAyZC1mMmU0MmQxMDZhY2MiLCJ1c2VyX3V1aWQiOiIwMWNjOGNiZS1iMTYwLTRmNDMtOTFiMC02NDc1MjFhMWM4NzQifQ.9bldT7Ap7J-hccL2aAy1jgrLGQarsmEZbkbGhwzJdKLwYF_jQH2gr4fmMgKIb5z6_4rrgu9lfub1oze_I9-C0Q
-  // Calendly current organization: https://api.calendly.com/organizations/5a80c6c2-6761-4cf6-a806-f69be846ca07
   connectedCallback(){
     console.log('this.calendlyLink');
  
