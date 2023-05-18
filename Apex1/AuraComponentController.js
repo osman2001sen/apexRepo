674525d4ca19bb1e7388e9ae05e54c3a2f748072
osman2001sen <117099035+osman2001sen@users.noreply.github.com>
2023-05-18 14:17:06 -0400

    AuraComponentController.js

---
+++ Apex1/AuraComponentController.js
@@ -0,0 +1,12 @@
+({
+	changeMsg : function(component, event, helper) {
+        
+        console.log('Buton tıklandı!');
+        
+        //v.hellomsg
+        var msg = component.get("v.hellomsg");
+        
+        console.log(msg);
+		
+	}
+})
